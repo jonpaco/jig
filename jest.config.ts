99d191d8c1e57691d07e82cd7e110442708c2f41
Jon Cardinale <jon.cardinale@pm.me>
2026-04-03 17:11:46 -0400

    chore: scaffold pnpm monorepo

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  projects: [
+    '<rootDir>/packages/protocol',
+    '<rootDir>/packages/cli',
+  ],
+};
+
+export default config;
